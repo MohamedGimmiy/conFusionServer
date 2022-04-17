81125d266ac2b8620c9150d32cf2dff9ec778b05
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-04-17 16:56:21 +0200

    upload files using multer module

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
 // redirect all traffic to secure server
@@ -51,6 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
+app.use('/imageUpload', uploadRouter)
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes')
