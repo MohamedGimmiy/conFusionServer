169926db756332c83e8285b7b272ca264d7c7b23
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-04-18 03:57:25 +0200

    passport facebook OAuth.2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-09876-45678-34578',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '728212415198891',
+        clientSecret: 'd8e76002542e6b4e993fcd5d975c4d7e'
+    }
 }
